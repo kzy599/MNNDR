75dbee0af8a39a47a94c9923a31257fb861a3da4
Ziyi Kang <113572457+kzy599@users.noreply.github.com>
2025-06-18 17:04:40 +0800

    Update analyzeResult.r

--- SimulatedDatasets/analyzeResult.r
+++ SimulatedDatasets/analyzeResult.r
@@ -2,7 +2,7 @@ library(data.table)
 library(ggplot2)
 library(ggsci)
 library(dplyr)
-
+library(multcompView)
 calpercent = function(x,y){
   x = round(x,2)
   y = round(y,2)
@@ -45,7 +45,22 @@ for(v in value_type){
       if(v %in% c("wcov","wva","fwcova","wfac") & m == "PBLUP") next
 
       if(v %in% c("accov","ac","va")) temp_dt = pdt else temp_dt = wdt
-      
+
+      #向量化实现，可以节省关于model和type的两个for循环
+      # if(v %in% c("wcov","wva","fwcova","wfac")) temp_dt = temp_dt[MODE!="PBLUP",]
+
+      # vsd = paste(v,"_sd",sep = "")
+
+      # temp_value = temp_dt[,..v]
+
+      # temp_value_sd = temp_dt[,..vsd]
+
+      # temp_output = data.table(Vtype = v,MODE = temp_dt$MODE,type = temp_dt$type,value = temp_value, value_sd = temp_value_sd)
+
+      #colnames(temp_output) = c("Vtype","MODE","type","value","value_sd")
+
+      # output = rbind(output, temp_output)
+
       vsd = paste(v,"_sd",sep = "")
 
       temp_value = temp_dt[MODE ==m & type==t,..v]
@@ -61,6 +76,40 @@ for(v in value_type){
 }
 return(output)
 }
+
+
+checking_rep = function(wac_mean_sd,pac_mean_sd,cal){
+
+wdt = wac_mean_sd[combine == "original",]
+pdt = pac_mean_sd[combine == "original",]
+
+# if(cal == "ACC") model = c("PBLUP","GBLUP","FCN","CNN","MNN") else model = c("GBLUP","FCN","CNN","MNN")
+model = c("PBLUP","GBLUP","FCN","CNN","MNNDR")
+type = c("ST","MT_continuous","MT_binary")
+if(cal == "COV") value_type = c("accov","fcov","wcov","fwcov","wfcov")
+if(cal == "VAR") value_type = c("va","fva","wva","fwcova")
+if(cal == "ACC") value_type = c("ac","famacc","wfac")
+output = data.table()
+for(v in value_type){
+  for(m in model){
+    for(t in type){
+
+      if(v %in% c("wcov","wva","fwcova","wfac") & m == "PBLUP") next
+
+      if(v %in% c("accov","ac","va")) temp_dt = pdt else temp_dt = wdt
+
+      temp_value = temp_dt[MODE ==m & type==t,..v]
+
+      temp_output = data.table(Vtype = v,MODE = m,type = t,value = temp_value[[1]])
+
+      output = rbind(output,temp_output)
+
+    }
+  }
+}
+return(output)
+}
+
 theme_zg <- function(..., bg='white'){
   require(grid)
   theme_classic(...) +
@@ -86,10 +135,7 @@ theme_zg <- function(..., bg='white'){
           legend.background = element_rect( linetype="solid",colour ="black")
     )
 }
-pac_mean_sd = fread("pac_mean_sd.csv",sep = ",")
-wac_mean_sd = fread("wac_mean_sd.csv",sep = ",")
-pac_mean_sd <- classModel(pac_mean_sd)
-wac_mean_sd = classModel(wac_mean_sd)
+
 
 pac = fread("pac.csv",sep = ",")
 pac_mean_sd = fread("pac_mean_sd.csv",sep = ",")
@@ -97,35 +143,6 @@ setorder(pac_mean_sd,ac)
 pac_mean_sd
 pac_mean_sd[!mname%flike%"_a",]
 t.test(x = pac[mname =="mul_linear_a",ac],y = pac[mname=="gblup_mt",ac])
-
-pv = "ac"
-pvse = "ac_sd"
-
-pac_mean_sd <- classModel(pac_mean_sd)
-zt = pac_mean_sd[combine == "original",]
-zt$value = zt[,..pv]
-zt$valuese = zt[,..pvse]
-zt$type = factor(zt$type,level = c("ST","MT_continuous","MT_binary"))
-zt$MODE = factor(zt$MODE,level = c("PBLUP","GBLUP","FCN","CNN","MNNDR"))
-P <- ggplot(data = zt, aes(x = MODE, y = value, group = type, fill = type)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.9),width = 0.9) +  # 确保条形图分组不重叠
-  geom_text(aes(label = round(value,2)), position = position_dodge(width = 0.9),vjust = -1, size = 4) +  # 添加数值标签
-  xlab("Model") +
-  ylab("Population-wide accuracy") +
-  theme_zg() +
-  theme(legend.position ="right", legend.title = element_blank()) +
-  geom_errorbar(aes(ymin = value - valuese, ymax = value + valuese),
-                position = position_dodge(width = 0.9),  # 与条形图对齐
-                width = 0.2, alpha = 0.5) +
-  labs(fill = "Mat") +
-  scale_fill_aaas()
-
-ggsave("Figrue_pac_pure.pdf", P , width = 15, height = 6, dpi = 300)
-ggsave("Figrue_pac_low.pdf", P , width = 15, height = 6, dpi = 300)
-ggsave("Figrue_pac_medium.pdf", P , width = 15, height = 6, dpi = 300)
-ggsave("Figrue_pac_high.pdf", P , width = 15, height = 6, dpi = 300)
-
-pac = classModel(pac)
 
 #检测模型之间的差异
 anova_model<- aov(ac~MODE,data = pac[combine == "original"&type == "ST",] )
@@ -390,8 +407,16 @@ Me = "PBLUP"
 t.test(x = wac[combine == "original"&MODE ==Me&type == "ST",famacc],
 y = wac[combine == "original"&MODE ==Me&type == "MT_continuous",famacc])
 
+#=============================================================================
+pac = fread("pac.csv",sep = ",")
+wac = fread("wac.csv",sep = ",")
+pac = classModel(pac)
+wac = classModel(wac)
 
-
+pac_mean_sd = fread("pac_mean_sd.csv",sep = ",")
+wac_mean_sd = fread("wac_mean_sd.csv",sep = ",")
+pac_mean_sd <- classModel(pac_mean_sd)
+wac_mean_sd = classModel(wac_mean_sd)
 
 #checking
 zt = checking(wac_mean_sd,pac_mean_sd,cal = "ACC")
@@ -476,3 +501,102 @@ ggsave("Figrue_cov_pure.pdf", P , width = 15, height = 6, dpi = 300)
 ggsave("Figrue_cov_low.pdf", P , width = 15, height = 6, dpi = 300)
 ggsave("Figrue_cov_medium.pdf", P , width = 15, height = 6, dpi = 300)
 ggsave("Figrue_cov_high.pdf", P , width = 15, height = 8, dpi = 300)
+
+
+
+
+pac_mean_sd = fread("pac_mean_sd.csv",sep = ",")
+wac_mean_sd = fread("wac_mean_sd.csv",sep = ",")
+pac_mean_sd <- classModel(pac_mean_sd)
+wac_mean_sd = classModel(wac_mean_sd)
+
+pac = fread("pac.csv",sep = ",")
+wac = fread("wac.csv",sep = ",")
+pac = classModel(pac)
+wac = classModel(wac)
+
+value_type = c("ac","famacc","wfac")
+type = c("ST","MT_continuous","MT_binary")
+output_test = data.table()
+
+for(t in type){
+
+for(v in value_type){
+
+  if(v == "ac") temp_dt = pac[type == t,] else temp_dt = wac[type == t,]
+
+  if(v == "wfac") temp_dt = temp_dt[MODE!="PBLUP",]
+  
+  aov_res <- aov(as.formula(paste(v, "~ MODE")), data = temp_dt)
+  
+  tukey_res <- TukeyHSD(aov_res,conf.level = 0.95)
+  
+  letters <- multcompLetters4(aov_res, tukey_res)
+  
+  output_test = rbind(output_test,data.table(MODE = names(letters$MODE$Letters),Letter = letters$MODE$Letters,type = t,Vtype = v))
+   
+}
+
+}
+
+# df = checking_rep(wac,pac,cal="ACC")
+
+# output_test <- data.table()
+# for(t in unique(df$type)){
+# for (g in unique(df$Vtype)) {
+
+#   sub <- df %>% filter(Vtype == g, type == t)
+  
+#   # Run ANOVA + Tukey
+#   aov_res <- aov(value ~ MODE, data = sub)
+#   tukey_res <- TukeyHSD(aov_res)
+#   letters <- multcompLetters4(aov_res, tukey_res)
+  
+#   letter_df <- data.frame(
+#     MODE = names(letters$MODE$Letters),
+#     Letter = letters$MODE$Letters,
+#     Vtype = g,
+#     type = t
+#   )
+  
+#   output_test <- bind_rows(output_test, letter_df)
+# }
+# }
+zt = checking(wac_mean_sd,pac_mean_sd,cal = "ACC")
+zt1 <- merge(output_test, zt, by = c("MODE", "type","Vtype"), all = FALSE)
+zt1$MODE = factor(zt1$MODE,level = c("PBLUP","GBLUP","FCN","CNN","MNNDR"))
+zt1$type = factor(zt1$type,level = c("ST","MT_continuous","MT_binary"))
+zt1[Vtype == "ac",Vtype:="Individual"]
+zt1[Vtype == "famacc",Vtype:="Family"]
+zt1[Vtype == "wfac",Vtype:="Within-family"]
+zt1$Vtype = factor(zt1$Vtype,level = c("Individual","Family","Within-family"))
+zt1[, letter_y_value := value + 1.1 * value_sd, by = Vtype]
+zt1[, letter_y_test := value + 3.1 * value_sd, by = Vtype]
+P <- ggplot(data = zt1[type == "ST",], aes(x = MODE, y = value, group = MODE, fill = MODE)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9),width = 0.9) +  # 确保条形图分组不重叠
+  geom_text(aes(label = round(value,2),y = letter_y_value), position = position_dodge(width = 0.9),vjust = 0, size = 4) +  # 添加数值标签
+  xlab("Model") +
+  ylab("Accuracy") +
+  theme_zg() +
+  theme(legend.position ="right", legend.title = element_blank()) +
+  geom_errorbar(aes(ymin = value - value_sd, ymax = value + value_sd),
+                position = position_dodge(width = 0.9),  # 与条形图对齐
+                width = 0.2, alpha = 0.5) +
+  labs(fill = "Mat") +
+  scale_fill_aaas()+geom_text(aes(label = Letter, y = letter_y_test), position = position_dodge(width = 0.9),vjust = 0) + facet_wrap(~type + Vtype, scales = "free")+
+  ggtitle(label = "ST and MT prediction across models",subtitle = "Simulated dataset (high non-additive)") +
+  theme(
+    plot.title = element_text(
+      size = 20,          # 大字号
+      face = "bold",      # 加粗
+      hjust = 0.5,        # 居中
+      color = "black"     # 颜色（可选）
+    ),
+    # 调整x轴标签字体
+    axis.title.x = element_text(size = 16, face = "bold", color = "black"),
+    # 调整y轴标签字体
+    axis.title.y = element_text(size = 16, face = "bold", color = "black"),
+    #调整副标题
+    plot.subtitle = element_text(size = 12, hjust = 0.5, face = "italic")
+  )
+ggsave("Figrue_ac_high.pdf", P , width = 15, height = 6, dpi = 300)
